fb155fe74fc100efc69b8e6c7f42509d43fa8585
Aaron Xu <omicron3069@protonmail.com>
2018-07-06 15:54:21 +0800

    perfect...

--- Online-Shopping/EasyBuy/src/com/easybuy/biz/OrderBiz.java
+++ Online-Shopping/EasyBuy/src/com/easybuy/biz/OrderBiz.java
@@ -1,17 +1,21 @@
 package com.easybuy.biz;
 
 import com.easybuy.entity.Order;
+import com.easybuy.entity.OrderDetail;
+import com.easybuy.entity.User;
+import com.easybuy.util.ShoppingCart;
 
 import java.util.List;
 
 public interface OrderBiz {
-    void add (Order order);
 
-    void deleteById (Integer id);
-
-    Order getOrderById(Integer id);
-
-    Integer count (Integer userId);
+    Order payShoppingCart(ShoppingCart cart, User user, String address);
 
     List<Order> getOrderList(Integer userId, Integer currentPageNo, Integer pageSize);
+
+    Integer count(Integer userId);
+
+    List<OrderDetail> getOrderDetailList(Integer orderId);
+
+
 }
